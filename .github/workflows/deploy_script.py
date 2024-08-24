a72230ae320e8070c2a36baf23318176d6ca7cdc
Thomas Weich <tmsweich@gmail.com>
2024-08-24 19:51:49 +0200

    fixed authorization

--- .github/workflows/deploy_script.py
+++ .github/workflows/deploy_script.py
@@ -11,15 +11,25 @@ version = os.environ.get('VERSION', '1.0.0')
 # Set up OAuth 2.0 flow
 flow = Flow.from_client_secrets_file(
     'oauth_client_file.json',
-    scopes=['https://www.googleapis.com/auth/chromewebstore']
-)
+    scopes=['https://www.googleapis.com/auth/chromewebstore'],
+    redirect_uri='urn:ietf:wg:oauth:2.0:oob')
 
-# Run the flow
-creds = flow.run_local_server(port=0)
+# Generate the authorization URL
+auth_url, _ = flow.authorization_url(prompt='consent')
+
+print(f"Please visit this URL to authorize the application: {auth_url}")
+code = input("Enter the authorization code: ")
+
+# Exchange the authorization code for credentials
+flow.fetch_token(code=code)
+credentials = flow.credentials
+
+# Save the credentials for future use
+with open('token.json', 'w') as token:
+    token.write(credentials.to_json())
 
 # Build the service
-service = build('chromewebstore', 'v1.1', credentials=creds)
-
+service = build('chromewebstore', 'v1.1', credentials=credentials)
 # Upload the ZIP file
 with open('extension.zip', 'rb') as file:
     media_body = file.read()
